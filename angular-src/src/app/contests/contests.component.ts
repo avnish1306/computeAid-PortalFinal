b4793d359936fbafd555821fea2355a73bbb1c66
DebanjanDasMhapatra <debanjandm123@gmail.com>
2019-10-13 22:31:32 +0530

    Bug fixing started

--- angular-src/src/app/contests/contests.component.ts
+++ angular-src/src/app/contests/contests.component.ts
@@ -17,27 +17,27 @@ export class ContestsComponent implements OnInit {
     private authService: AuthService,
     private contestService: ContestService,
     private notificationsService: NotificationsService,
-    private localStorageService : LocalStorageService) { }
+    private localStorageService: LocalStorageService) { }
 
-    contests;
-    live = [];
-    id; rules; index: number = -1;
-    userid;
-    info = {
-      cname: "",
-      details: "",
-      startTime: "",
-      endTime: "",
-      duration: ""
-    };
-    stats = {
-      up: 0,
-      on: 0,
-      pa: 0
-    }
-    loading: boolean = true;
-    agree: boolean = false;
-    registering: boolean = false;
+  contests;
+  live = [];
+  id; rules; index: number = -1;
+  userid;
+  info = {
+    cname: "",
+    details: "",
+    startTime: "",
+    endTime: "",
+    duration: ""
+  };
+  stats = {
+    up: 0,
+    on: 0,
+    pa: 0
+  }
+  loading: boolean = true;
+  agree: boolean = false;
+  registering: boolean = false;
 
   ngOnInit() {
     this.live = [];
@@ -45,7 +45,7 @@ export class ContestsComponent implements OnInit {
     this.contestService.getAllContests().subscribe(
       data => {
         this.contests = data.contests;
-        if(this.contests.length > 0)
+        if (this.contests.length > 0)
           this.fetchRegister(0);
       },
       error => {
@@ -60,11 +60,11 @@ export class ContestsComponent implements OnInit {
       data => {
         this.contests[i].registered = data.isRegistered;
         this.contests[i].attempted = data.access;
-        this.contests[i].started = this.localStorageService.isStarted(this.userid,this.contests[i]._id);// data.started;
-        if(i < this.contests.length-1)
-          this.fetchRegister(i+1);
+        this.contests[i].started = this.localStorageService.isStarted(this.userid, this.contests[i]._id);// data.started;
+        if (i < this.contests.length - 1)
+          this.fetchRegister(i + 1);
         else {
-          for(let i=0;i<this.contests.length;i++) {
+          for (let i = 0; i < this.contests.length; i++) {
             var dt = new Date().getTime();
             let st = new Date(this.contests[i].startTime).getTime() <= dt;
             let end = new Date(this.contests[i].endTime).getTime() <= dt;
@@ -73,16 +73,16 @@ export class ContestsComponent implements OnInit {
               "ended": end,
               "enabled": st && !end
             });
-            if(st && !end)
+            if (st && !end)
               this.stats.on++;
-            if(!st)
+            if (!st)
               this.stats.up++;
-            if(end)
+            if (end)
               this.stats.pa++;
           }
           this.loading = false;
         }
-      },error => {
+      }, error => {
         this.notificationsService.create("", JSON.parse(error._body).error);
       }
     );
@@ -90,15 +90,16 @@ export class ContestsComponent implements OnInit {
 
   register(i: number) {
     this.registering = true;
+    this.loading = true;
     this.contestService.register(this.contests[i]._id).subscribe(
       data => {
-        if(data.status == 1) {
-          this.notificationsService.success("Done", "Registered Successfully !!!"); 
+        if (data.status == 1) {
+          this.notificationsService.success("Done", "Registered Successfully !!!");
           this.ngOnInit();
         }
         else
-          this.notificationsService.error("Oops", "Registration Failure !!!");        
-      },error => {
+          this.notificationsService.error("Oops", "Registration Failure !!!");
+      }, error => {
         this.notificationsService.error("", JSON.parse(error._body).error);
       }
     );
@@ -108,8 +109,8 @@ export class ContestsComponent implements OnInit {
     var dt = new Date().getTime();
     let st = new Date(this.contests[i].startTime).getTime() <= dt;
     let end = new Date(this.contests[i].endTime).getTime() <= dt;
-    if(st && !end)
-      this.router.navigate(['/ques/contests/'+this.contests[i]._id]);
+    if (st && !end)
+      this.router.navigate(['/ques/contests/' + this.contests[i]._id]);
     else {
       alert('This contest has now ended. Talk to the coordinators.');
       this.ngOnInit();
@@ -117,15 +118,15 @@ export class ContestsComponent implements OnInit {
   }
 
   editContest(i: number) {
-    this.router.navigate(['/contests/add/'+this.contests[i]._id]);
+    this.router.navigate(['/contests/add/' + this.contests[i]._id]);
   }
 
   addQuestion(i: number) {
-    this.router.navigate(['/ques/add/'+this.contests[i]._id+'/new']);
+    this.router.navigate(['/ques/add/' + this.contests[i]._id + '/new']);
   }
 
   rankings(i: number) {
-    this.router.navigate(['/contests/ranking/'+this.contests[i]._id]);
+    this.router.navigate(['/contests/ranking/' + this.contests[i]._id]);
   }
 
   stageInfo(i: number) {
@@ -141,21 +142,23 @@ export class ContestsComponent implements OnInit {
     this.id = this.contests[i].name;
     this.index = i;
   }
-  
+
   deleteContest(i: string) {
-    if(confirm('Sure to delete contest? This action can\'t be undone'))
-    this.contestService.deleteContest(i).subscribe(
-      data => {
-        this.notificationsService.success("Success", data.msg, {timeOut: 5000, showProgressBar: true, pauseOnHover: true, clickToClose: true, animate: 'fromRight'});
-        this.ngOnInit();
-      },
-      error => {
-        this.notificationsService.error("Oops!!", JSON.parse(error._body).error, {timeOut: 5000, showProgressBar: true, pauseOnHover: true, clickToClose: true, animate: 'fromRight'});
-      }
-    );
+    if (confirm('Sure to delete contest? This action can\'t be undone')) {
+      this.loading = true;
+      this.contestService.deleteContest(i).subscribe(
+        data => {
+          this.notificationsService.success("Success", data.msg, { timeOut: 5000, showProgressBar: true, pauseOnHover: true, clickToClose: true, animate: 'fromRight' });
+          this.ngOnInit();
+        },
+        error => {
+          this.notificationsService.error("Oops!!", JSON.parse(error._body).error, { timeOut: 5000, showProgressBar: true, pauseOnHover: true, clickToClose: true, animate: 'fromRight' });
+        }
+      );
+    }
   }
 
-  isAdmin(){
+  isAdmin() {
     return this.authService.isAdmin();
   }
 
